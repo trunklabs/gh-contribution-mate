f0fd80689c93273504c432bb1f0045db74aa5a2c
Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
2022-12-22 02:11:15 +0900

    refactor: extract global details to config.json

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
+import config from "./config.json" assert { type: "json" };
 import { Command } from "./deps.ts";
 
 await new Command()
-  .name("contribution-mate")
-  .version("0.1.0")
-  .description(
-    "Contribution Mate is a GitHub CLI extension that allows you to safely synchronize your commit history of non-GitHub repositories with your GitHub account without fear of exposing the source code."
-  )
+  .name(config.name)
+  .version(config.version)
+  .description(config.description)
   .action(() => console.log("Hello World, my name is Contribution Mate!"))
   .parse(Deno.args);
