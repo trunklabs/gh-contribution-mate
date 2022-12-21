654a4d1ccaee54e6be37f70a802e8944bc38c8c9
Alex Marinkevych <24780101+alexey-marinkevich@users.noreply.github.com>
2022-12-21 17:05:43 +0100

    feat: setup cliffy

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-console.log("Hello World, my name is Contribution Mate!");
+import { Command } from "../deps.ts";
+
+await new Command()
+  .name("contribution-mate")
+  .version("0.1.0")
+  .description(
+    "Contribution Mate is a GitHub CLI extension that allows you to safely synchronize your commit history of non-GitHub repositories with your GitHub account without fear of exposing the source code."
+  )
+  .action(() => console.log("Hello World, my name is Contribution Mate!"))
+  .parse(Deno.args);
