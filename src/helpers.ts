5509651c2c651e12c9b24de594d366e925d60860
Alexey <24780101+alexey-marinkevich@users.noreply.github.com>
2023-01-05 18:46:05 +0100

    Add helper for running cli

--- src/helpers.ts
+++ src/helpers.ts
@@ -9,3 +9,21 @@ export const fileExists = async (file: string): Promise<boolean> => {
     throw e;
   }
 };
+
+export async function runCli(params: string) {
+  const props = params.split(" ");
+
+  const process = Deno.run({
+    cmd: props,
+    stdout: "piped",
+    stderr: "piped",
+  });
+
+  const { code } = await process.status();
+  if (code === 0) {
+    return new TextDecoder().decode(await process.output()).trim();
+  } else {
+    const error = new TextDecoder().decode(await process.stderrOutput());
+    console.error(error);
+  }
+}
