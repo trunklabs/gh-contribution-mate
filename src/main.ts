e6b335aee67a7bab2dcb3dd5d32515154a1e15e2
Alexey <24780101+alexey-marinkevich@users.noreply.github.com>
2023-01-06 16:13:25 +0100

    feat: add configuration command logic

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import config from "./config.json" assert { type: "json" };
+import configuration from "./config.json" assert { type: "json" };
 import { Command } from "./deps.ts";
 
+import { configCommand } from "./commands/index.ts";
+
 await new Command()
-  .name(config.name)
-  .version(config.version)
-  .description(config.description)
+  .name(configuration.name)
+  .version(configuration.version)
+  .description(configuration.description)
   .action(() => console.log("Hello World, my name is Contribution Mate!"))
+  .command("config", configCommand)
   .parse(Deno.args);
