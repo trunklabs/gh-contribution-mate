6b6877309321903cce5454cee06f7057d713ed8f
Alexey <24780101+alexey-marinkevich@users.noreply.github.com>
2023-01-12 14:52:00 +0100

    Update tryDecode function

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -10,21 +10,28 @@ export const fileExists = async (file: string): Promise<boolean> => {
   }
 };
 
-export const tryDecode = async (params: string[]) => {
+interface DecodeParams {
+  silent: boolean;
+}
+
+export const tryDecode = async (params: string[], options?: DecodeParams) => {
   const process = Deno.run({
     cmd: params,
     stdout: "piped",
     stderr: "piped",
   });
-  const { code } = await process.status();
 
-  if (code !== 0) {
-    const error = new TextDecoder().decode(await process.stderrOutput());
-    console.error(
-      `[%cERROR%c] Process failed with an error: ${error}`,
-      "color: red",
-      "color: inherit",
-    );
+  if (!options?.silent) {
+    const { code } = await process.status();
+    if (code !== 0) {
+      const error = new TextDecoder().decode(await process.stderrOutput());
+      console.error(
+        `[%cERROR%c] Process failed with an error: ${error}`,
+        "color: red",
+        "color: inherit",
+      );
+    }
   }
+
   return new TextDecoder().decode(await process.output()).toString().trim();
 };
