8821159f3985426e14025ccc2a47d09373739d13
Alexey <24780101+alexey-marinkevich@users.noreply.github.com>
2023-01-14 17:48:01 +0100

    chore: move scripts to vr

--- script/commitlint.ts
+++ script/commitlint.ts
@@ -1,11 +1,11 @@
-import lint from "npm:@commitlint/lint@17.4.0";
-import conventionalConfig from "npm:@commitlint/config-conventional@17.4.0";
-import format from "npm:@commitlint/format@17.4.0";
+import lint from 'npm:@commitlint/lint@17.4.0';
+import conventionalConfig from 'npm:@commitlint/config-conventional@17.4.0';
+import format from 'npm:@commitlint/format@17.4.0';
 
 const message = await Deno.readTextFile(Deno.args[0]);
 
 if (!message) {
-  console.error(new Deno.errors.InvalidData("No commit message found"));
+  console.error(new Deno.errors.InvalidData('No commit message found'));
   Deno.exit(1);
 }
 
