815af86b2844cab35e232fc46bfba1183b5a5a34
Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
2023-01-14 23:17:16 +0900

    chore: lint commits with commitlint (#19)
    * chore: lint commits with commitlint
    * fix: typo
    
    Closes #6
    
    - Added custom script with commitlint integration to lint commits
    - Updated contributing guide
    - Replaced deno tasks with velociraptor scripts
    
    Co-authored-by: Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
    Co-authored-by: Alexey <24780101+alexey-marinkevich@users.noreply.github.com>

---
+++ script/commitlint.ts
@@ -0,0 +1,20 @@
+import lint from "npm:@commitlint/lint@17.4.0";
+import conventionalConfig from "npm:@commitlint/config-conventional@17.4.0";
+import format from "npm:@commitlint/format@17.4.0";
+
+const message = await Deno.readTextFile(Deno.args[0]);
+
+if (!message) {
+  console.error(new Deno.errors.InvalidData("No commit message found"));
+  Deno.exit(1);
+}
+
+const result = await lint.default(
+  message,
+  conventionalConfig.rules,
+);
+
+if (!result.valid) {
+  console.log(format.format({ results: [result] }, { color: true }));
+  Deno.exit(1);
+}
