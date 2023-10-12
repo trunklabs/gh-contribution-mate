b3d3e4b029674eca1680bbd18233c72134ed1fdb
Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
2023-10-12 22:40:30 +0900

    feat: implement logging

--- src/commands/sync.ts
+++ src/commands/sync.ts
@@ -1,6 +1,6 @@
 import { colors, Command, Select } from 'cliffy';
 import { join } from 'std/path';
-import { ghNoReplyCheck, promptAllInputs, validate } from 'lib';
+import { ghNoReplyCheck, logger, promptAllInputs, validate } from 'lib';
 import { getConfig, getConfigDir, setConfig } from '../config.ts';
 import {
   AuthorSchema,
@@ -22,11 +22,17 @@ const SYNC_REPO_PATH = join(getConfigDir(), 'sync-repo');
 
 export default new Command()
   .description('Synchronize commits from local repositories to GitHub profile.')
-  .action(async () => {
+  .option('-d, --debug', 'Enable debug output.')
+  .action(async ({ debug }) => {
+    if (debug) logger.enable();
+
     let config = await getConfig();
+    logger.info('Current config:', config);
 
     if (!config.syncRepo) {
+      logger.info('No sync repository found. Cloning one...');
       const repos = await getRepositories();
+      logger.info('Found repositories:', repos);
 
       const id = await Select.prompt({
         message: 'Choose a repository to sync commits to:',
@@ -35,15 +41,23 @@ export default new Command()
         options: repos.map((repo) => ({ value: repo.id, name: repo.name })),
       });
       const selectedRepo = repos.find((repo) => repo.id === id);
+      logger.info('Selected repository:', selectedRepo);
       await cloneRepository(selectedRepo!, 'sync-repo', getConfigDir());
       await setConfig({ syncRepo: selectedRepo });
       config = await getConfig();
+      logger.info('New config:', config);
     } else {
+      logger.info('Sync repository found. Pulling latest changes...');
       await pullRepositoryChanges(SYNC_REPO_PATH);
     }
 
     if (!config.author) {
+      logger.info('No author found. Prompting one...');
       const currentGitAuthor = await getGitAuthor();
+      logger.info(
+        'Current git author configured in git config:',
+        currentGitAuthor,
+      );
       const [name, email] = await promptAllInputs(
         Object.keys(AuthorSchema.shape).map((key) => ({
           message: `Authoring ${key}:`,
@@ -59,11 +73,14 @@ export default new Command()
       ghNoReplyCheck(email);
 
       const author: AuthorType = { name, email };
+      logger.info('Selected author:', { name, email });
       await setConfig({ author });
       config = await getConfig();
+      logger.info('New config:', config);
     }
 
     const history = await getGitHistory(SYNC_REPO_PATH);
+    logger.info('Current history:', history);
     const authorsWithDirs: Array<AuthorType & { dir: string }> = Object.values(
       config.repos,
     ).flatMap((repo) =>
@@ -76,6 +93,7 @@ export default new Command()
     )).flat().filter((commit) =>
       !history.some((entry) => entry?.hash === commit.hash)
     );
+    logger.info('New commits:', commits);
 
     if (!commits.length) {
       console.log('No new commits to synchronize. Exiting...');
@@ -88,12 +106,14 @@ export default new Command()
     );
 
     commits.forEach((commit) => {
+      logger.info('Creating commit:', commit);
       createCommit(
         { ...commit, author: config.author! },
         SYNC_REPO_PATH,
       );
     });
 
+    logger.info('Pushing changes to sync repository...');
     await pushRepositoryChanges(SYNC_REPO_PATH);
 
     console.log(
